6111a8f74cf3f70539a32202e16bf880ef300bee
skobyl <59086712+skobyl@users.noreply.github.com>
2020-06-10 16:44:29 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 
 from create_vectors import *
-from structure_wsd import *
+from structure_wsd_s import *
 import argparse
 import numpy as np
+import random
 
 parser = argparse.ArgumentParser()
 parser.add_argument("verbe", help = "abattre, aborder, affecter, comprendre, compter")
@@ -106,32 +107,106 @@ for i in espace_vectorielle:
 	print(i.get_gold_class())
 	print('\t')
 
+E = 30 # nombre d'époques pour tourner l'algo
 
-classification = structure_wsd_s.KMeans(vectors, num_senses, None, "cosine") # 6 clusters pour abattre; à remplacer ensuite par le nombre de sens
-print(len(classification.examples))
+matrix=examples.get_espace_vec()
+print(type(matrix))
+random.shuffle(matrix)
+print(type(matrix))
+print(len(matrix))
+print(matrix[0])
+print(examples.espace_vec[0].get_index())
+index = examples.espace_vec[0].get_index()
+print(examples.espace_vec[0].get_vector())
+vector = examples.espace_vec[0].get_vector()
+print(examples.espace_vec[0].get_gold_class())
+#print("vérification fonctions ")
+#print(Examples.get_Ovector_by_id(index).vector)
+#print(Examples.get_Ovector_by_vector(vector).index)
+#print(Examples.get_Ovector_by_vector(vector).gold)
+#for i in matrix:
+#	print(i.get_index())
+#	print(i.get_vector())
+#	print(i.get_gold_class())
+#	print('\t')
+
+# on calcule les occurrences des senses
+# dans le as où l'exemple se trouve à la même distance de plusieurs clusters, 
+# on va choisir celui dont le sens est le plus fréquent
+senses = Counter(int(line) for line in file_gold)
+#print("SENSES ", senses)
+#print(senses.keys())
+#print(len(senses.keys()))
+N = len(senses.keys()) # le nb de clusters souhaité
+GOLD = senses.keys() # les numéros des sens, les classes gold
+classification = KMeans(examples.espace_vec, N, GOLD, None, "cosine") # 6 clusters pour abattre; à remplacer ensuite par le nombre de sens
+#print(len(classification.examples))
 clusters = classification.create_empty_clusters()
-for i,cluster in clusters.items():
-	print(cluster.center)
-print(len(classification.examples))
-distance = classification.distance_matrix("cosine")
-#print(len(distance))
-#for i in range(classification.k):
-#	for  j in range(len(classification.examples)):
-#min_distance = np.where(distance == np.amin(distance))
-#distance = np.array([[11, 12, 13],
- #                        [14, 15, 16],
-  #                       [17, 15, 11],
-   #                      [12, 14, 15]])
-minValue = np.amin(distance)
-print(minValue)
-min_distance = np.where(distance == np.amin(distance))
-#print('Tuple of arrays returned : ', min_distance)
-print('List of coordinates of minimum value in Numpy array : ')
-		 # zip the 2 arrays to get the exact coordinates
-listOfCordinates = list(zip(min_distance[0], min_distance[1]))
-		 # travese over the list of cordinates
-for cord in listOfCordinates:
-	print(cord)
-	# ensuite il faut regarder les exemples à chaque coordonnée et ajouter l'exemple au cluster correspondant
-	# il faut vérifier quoi faire si un exo est à la même distance de deux clusters
+print("CLUSTER INITIALISATION ")
+print(classification.clusters)
+for idx in classification.clusters:
+	print(classification.clusters[idx].center)
+	print(classification.clusters[idx].id)
+
+#print(len(classification.examples))
+
+for i in range(E): # nombre d'époques à définir
+#	print("RUN ", i)
+	#for cluster in classification.clusters:
+
+	distance = classification.distance_matrix("cosine")
+	for j in range(len(distance.T)): # on parcourt les exemples ; il faut savoir à quel id des exemples correspond j
+		min_value_index = np.argmin(distance.T[j]) # on trouve l'indice de la valeur min; c'est l'id du CLUSTER
+		#print(distance.T[j])
+		#print("MIN VALUE INDEX = ", min_value_index)
+		#print(min_value_index)
+		#exo = classification.examples[j]
+		exo = matrix[j] # exo à ajouter dans le cluster #min_value_index
+		#print("EXO IDX ", exo.index)
+		#print("type exo")
+		#print(type(exo))
+		if exo != classification.clusters[min_value_index].initial_example :
+			classification.clusters[min_value_index].add_example_to_cluster(exo) # j = quel id de l'exemple ?
+	for cluster in classification.clusters:
+		#print("EXOS = ", len(classification.clusters[cluster].examples))
+		#print("OLD center = ", classification.clusters[cluster].center)
+		classification.clusters[cluster].recalculate_center()
+		#print("NEW CENTER = ", classification.clusters[cluster].center)
+		if i < E-1 : # on va supprimer les exemples des clusters jusqu'au dernier run
+			classification.clusters[cluster].delete_examples()
+
+# on vérifie le résultat
+for i in classification.clusters:
+	print("CLUSTER ", i)
+	print(len(classification.clusters[i].examples))
+	print(Counter([exo.gold for exo in classification.clusters[i].examples]))
+
+
+# classification = structure_wsd_s.KMeans(vectors, num_senses, None, "cosine") # 6 clusters pour abattre; à remplacer ensuite par le nombre de sens
+# print(len(classification.examples))
+# clusters = classification.create_empty_clusters()
+# for i,cluster in clusters.items():
+# 	print(cluster.center)
+# print(len(classification.examples))
+# distance = classification.distance_matrix("cosine")
+# #print(len(distance))
+# #for i in range(classification.k):
+# #	for  j in range(len(classification.examples)):
+# #min_distance = np.where(distance == np.amin(distance))
+# #distance = np.array([[11, 12, 13],
+#  #                        [14, 15, 16],
+#   #                       [17, 15, 11],
+#    #                      [12, 14, 15]])
+# minValue = np.amin(distance)
+# print(minValue)
+# min_distance = np.where(distance == np.amin(distance))
+# #print('Tuple of arrays returned : ', min_distance)
+# print('List of coordinates of minimum value in Numpy array : ')
+# 		 # zip the 2 arrays to get the exact coordinates
+# listOfCordinates = list(zip(min_distance[0], min_distance[1]))
+# 		 # travese over the list of cordinates
+# for cord in listOfCordinates:
+# 	print(cord)
+# 	# ensuite il faut regarder les exemples à chaque coordonnée et ajouter l'exemple au cluster correspondant
+# 	# il faut vérifier quoi faire si un exo est à la même distance de deux clusters
 
--- structure_wsd_s.py
+++ structure_wsd_s.py
@@ -3,49 +3,81 @@ import numpy as np
 from math import *
 import scipy.spatial
 
-class Exemple:
-	def __init__(self, example_number, gold_class):
-		self.vector=Ovector()
-		self.classe_gold=gold_class #prise du fichier .gold
-		self.indice=example_number #prise du fichier .tok_ids
-		self.example_matrix=np.empty((0,3), int) #pas encore bien definie
+class Examples:
+	def __init__(self):
+		self.espace_vec=[]
+
+	def set_vector_to_matrix(self,ovec):
+		self.espace_vec.append(ovec)
+
+	def get_espace_vec(self):
+		return self.espace_vec
+
+	def get_Ovector_by_id(self, id):
+		for example in self.espace_vec:
+			if example.index==id:
+				return example
+		print("Warning : example with id = ", id, " not found")
+		return None
 	
-	def add_to_matrix(self,indice,vector,example_matrix):
-		self.example_matrix[:,self.indice]=self.vector #remplacer le vecteur de cet exemple dans matrix
+	def get_Ovector_by_vector(self, vector):
+		for example in self.espace_vec :
+			if (example.vector == vector).all():
+				return example
+		print("Warning : example with vector = ", vector, " not found")
+		return None
 
 class Ovector:
-	
-	def __init__(self,list_traits,methode):
-		self.list_traits=list_traits
-		self.methode=methode #methode de fusion des vecteurs
+	def __init__(self,index,gold,methode=None,traits_syntaxique=None,traits_ngram=None):
+		self.traits_syntaxique=traits_syntaxique if traits_syntaxique is not None else np.zeros(traits_ngram.shape)#si initialize son valeur sinon np.zeros
+		self.traits_ngram= traits_ngram if traits_ngram is not None else np.zeros(traits_syntaxique.shape)
+		self.methode=methode if methode is not None else None #methode de fusion des vecteurs
+		self.index=index
+		self.gold=gold
+		self.vector=[[]]
 
 	def fusion_traits(self): #comment on va fusionner les traits pour créer un seul vecteur par exemple
-		if self.methode == 'somme' :
-			for i in self.traits:
-				vector_traits=np.array(i)
+		
+		if self.methode.lower() == 'somme' :
+			self.vector=np.add(self.traits_syntaxique,self.traits_ngram)
 
-		if self.methode == 'moyenne':
-			for i in self.traits:
-				vector_traits=np.array(i)
+		if self.methode.lower() == 'moyenne':
+			moyenne_syntx=np.mean(self.traits_syntaxique)
+			moyenne_ngram=np.mean(self.traits_ngram)
+			self.vector=np.array([moyenne_syntx,moyenne_ngram]) #on cree un vecteur de taille (2,)
 
-		if self.methode == 'concetanation' :
-			for i in self.traits:
-				vector_traits=np.concetenate(vector_traits,np.array(i))
+		if self.methode.lower() == 'concat' :
+			self.vector=np.concatenate((self.traits_syntaxique,self.traits_ngram),axis=None)
 
-		return vector_traits
+	def set_vector(self,vec):
+		self.vector=vec
+
+	def get_vector(self):
+		return self.vector
+
+	def get_gold_class(self):
+		return self.gold
+
+	def get_index(self):
+		return self.index
 
 class Cluster:
 	""" Le class de l'objet cluster """
-	def __init__(self,id_cluster,examples):
+	def __init__(self,id_cluster, initial_example, examples):
 		"""Args:
 			id: le id de cluster sois int sois str -> id correspond au id du sens
 			examples: les exemples appertenant à cluster
 		"""
+		# AJOUTER ATTRIBUT GOLD
 		self.id=id_cluster
+		self.initial_example = initial_example
+		#self.gold = gold
 		#self.examples=examples  #les exemples associées à cette cluster de sens, type:matrix
 		self.examples = []
 		self.examples.append(examples)
-		self.center = np.mean(self.examples, axis = 0)
+		self.center = np.mean([ example.vector for example in self.examples], axis=0)
+		print("SELF CENTER ", self.center)
+		#self.center = np.mean(self.examples, axis = 0)
 	
 	def add_example_to_cluster(self,example):
 		#self.examples = np.append(self.examples, example)
@@ -54,15 +86,17 @@ class Cluster:
 		#self.center = np.mean(self.examples, axis = 0)
 	
 	def recalculate_center(self):
-		self.center = np.mean(self.examples, axis=0)
+		self.center = np.mean([ example.vector for example in self.examples], axis=0)
 	
 	def delete_examples(self):
 		self.examples = []
+		self.examples.append(self.initial_example)
 
 class KMeans:
-
-	def __init__(self, examples, k,contraints,distance_formula):
+	_ID = 0
+	def __init__(self, examples, k, gold, contraints,distance_formula):
 		self.k=k
+		self.gold = gold # les noms des clusters=classes gold
 		self.conraints=contraints
 		self.distance_formula=distance_formula
 		self.examples=examples #pas encore bien etablie
@@ -71,9 +105,20 @@ class KMeans:
 		""" on initialis les k cluster avec le centre et un exemple
 		"""
 		# avant d'appliquer cette méthode il faut faire shuffle des exemples
-		for i in range(self.k):
-			self.clusters[i] = Cluster(i, self.examples[i]) #, self.examples[i])
-			#self.examples = np.delete(self.examples, i, axis=0)
+		print("GOLD = ", self.gold)
+		for g in self.gold:
+			print(type(g))
+			for i in range(len(self.examples)):
+				#print(self.examples[i].gold)
+				if int(self.examples[i].gold[0]) == g:
+					print("TRUE")
+					self.clusters[self._ID] = Cluster(g, self.examples[i], self.examples[i]) # à changer ensuite gold -> int 
+					self._ID+=1
+					print(i)
+					break
+		#for i in range(self.k):
+		#	self.clusters[i] = Cluster(i, self.examples[i]) #, self.examples[i])
+		#	#self.examples = np.delete(self.examples, i, axis=0) # no need
 		return self.clusters
 
 	def delete_example(self, example): # PAS BESOIN 
@@ -86,50 +131,14 @@ class KMeans:
 	def distance_matrix(self, distance_formula):
 		"""distance_formula : string, ex cosine, euclidean, cityblock (for manhattan distance)
 		"""
-		centers = [self.clusters[i].center for i in range(self.k)]
+		centers = [self.clusters[i].center for i in range(self._ID)]#range(1, len(self.gold)+1)]
+		#print("LEN CENTER ")
+		#for center in centers:
+		#	print(len(center))
 		# IL FAUT PRENDRE EN COMPTE LES EXOS QUI NE SONT PAS DANS LES CLUSTERS ! 
-		return scipy.spatial.distance.cdist(centers, self.examples, distance_formula)
+		# return scipy.spatial.distance.cdist(centers, self.examples, distance_formula)
+		return scipy.spatial.distance.cdist(centers, [example.vector for example in self.examples], distance_formula)
 	
-	def retun_final_clusters(self):
-		pass
-class Hierarchy:
-
-	def __init__(self,examples,contraints,clustering_type, clusters):
-		self.contraints=contraints
-		self.examples=examples
-		self.clustering_type=clustering_type
-		if self.clustering_type == 'ascending': #si la methode est ascending on  a le nombre des exemples comme nombre des clusters
-			self.nbr_cluster=len(self.exemples)
-		if self.clustering_type == 'descanding': #si la methode est descanding, on commence par un seul grande cluster
-			self.nbr_cluster=1
-		self.clusters = clusters
-
-	def update_nbr_clusters(self,n):
-		"""Remettre à jour le nombre se clusters """
-		self.nbr_cluster=n
-
-	def initialize_clusters(self):
-		"""
-		Creer les objets de clusters dans le nombre qu'on a besoin
-		"""
-		#S
-		#for ex in self.examples:
-			#clusters.add
-		pass
-
-
-
-	def merge_clusters(self):
-		"""Methode pour fusionner les clusters si le methode est ascending """
-		pass
-
-	def demerge_clusters(self):
-		"""Methode pour defusionner les clusters si le méthode est descending """
-		pass
-
-	def distance_matrix(self):
-		pass
-
 	def retun_final_clusters(self):
 		pass
 
@@ -137,13 +146,28 @@ class WSD:
 	def __init__(self,method,examples):
 		self.method = method
 		self.exampls=examples
-	
-	def create_clustering(self,examples,k,contraints,distance_formula):
-		if self.method == "kmeans":
-			return KMeans(self.examples,k,contraints,distance_formula)
-		if self.method=='hierarchy':
-			return Hierarchy(self.examples,contraints,clustering_type)
 
 	def evaluate(self,examples):
-		pass
+		dict_scores=defaultdict(dict)
+		classes=Counter()
+		dict_vraies={}
+		precision=0.0
+		rappel=0.0
+		F_score=0.0
+		
+		for c in clusters:
+			for e in c.examples:
+				classes[e.gold]+=1 #on calcule le nombre de gold pour chaque classes
 
+		for c in clusters:
+			id_cluster=c.id
+			centre_cluster=c.center.gold #le gold de l'exemple de centre
+			vrai=0
+			for e in c.examples:
+				if e.gold==centre_cluster:
+					vrai+=1
+			dict_scores[id_cluster]['precision']=vrai/len(c.examples)
+			dict_scores[id_cluster]['rappel']= vrai/classes[centre_cluster]
+			dict_scores[id_cluster]['F']= 2* ((dict_scores[id_cluster]['precision']*dict_scores[id_cluster]['rappel']) / (dict_scores[id_cluster]['precision']+dict_scores[id_cluster]['rappel']))
+
+		return dict_scores
